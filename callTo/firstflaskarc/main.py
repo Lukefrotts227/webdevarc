a883445bc2cea5e5eed0db7004fe711b17545566
Ljf227 <ljfrotton@gmail.com>
2023-09-13 12:43:46 -0400

    further building on my first flask project

--- callTo/firstflaskarc/main.py
+++ callTo/firstflaskarc/main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 class Person:
     def __init__(self, name):
         self.name = name
+        self.new = True
+    def add_club(self, club): 
+        self.club = club
         self.new = False
     def write_to(self): 
         pass
@@ -18,18 +21,33 @@ data = {}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == 'POST' and (not name in data):
         name = request.form['name']
         name = name.lower()
         data[name] = Person(name)
-
         return redirect(url_for('next', name=name))
+    
+    elif request.method == 'POST' and name in data: 
+        return redirect(url_for('next', name=name)) 
+    
+    elif request.method == 'POST' and name in data and data[name].new == True: 
+        pass
+
     return render_template('index.html')
 
 @app.route('/next/<name>', methods=['GET', 'POST'])
 def next(name): 
     rename = name.title()
+    if request.method == 'POST':
+        club = request.form['club']
+        data[name].add_club(club)
+
     return render_template('next.html', name=rename)
+
+@app.route('/main/<name>', methods=['GET', 'POST'])
+def main(name):
+    pass
+
 
 
 if __name__ == '__main__':
