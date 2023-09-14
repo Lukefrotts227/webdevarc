ab86b6cf03e07911b5d5f8cec2664241159b4f00
Ljf227 <ljfrotton@gmail.com>
2023-09-14 13:31:30 -0400

    building on js skills

---
+++ Ordinary/improvejs/public/script.js
@@ -0,0 +1,12 @@
+function sayHi(){
+    alert("HI"); 
+}
+
+
+window.sayHi(); 
+
+// change the background color to red
+
+
+document.body.style.background = "red";
+setTimeout(() => document.body.style.background = "", 1000);
