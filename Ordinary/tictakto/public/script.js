052d262fc4624974eec67ac683b67510932ee8d9
Ljf227 <ljfrotton@gmail.com>
2023-09-15 13:23:17 -0400

    stop point for now

--- Ordinary/tictakto/public/script.js
+++ Ordinary/tictakto/public/script.js
@@ -1,16 +1,26 @@
 
 // here is the empty board
 const board = [
-['e', 'e', 'e'],
-['e', 'e', 'e'],
-['e', 'e', 'e']
+['', '', ''],
+['', '', ''],
+['', '', '']
 ]; 
 
 let currentPlayer = 'X'; 
 let GameOver = true; 
 
 const cells = document.querySelectorAll('.board td'); 
-cells.forEach(cell => cell.addEventListener('click', handleCellClick))
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+
+for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[row].length; col++) {
+        const cell = cells[row * 3 + col];
+        cell.textContent = board[row][col];
+    }
+}
+
+const resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', resetGame);
 
 
 function handleCellClick(event){
@@ -18,14 +28,38 @@ function handleCellClick(event){
     const row = cell.parentElement.rowIndex; 
     const col = cell.cellIndex;
 
+    const winner = checkOutcome(board, row, col); 
+    if(winner){
+        
+    }else{
+        currentPlayer = swapPlayer(currentPlayer); 
+    }
+
 }
 
-function canPut(board, pos){
+function canPut(board, row, col){
+    return board[row][col] === ''; 
+}
+
+function checkOutcome(board, row, col){
+
+
+    return null; 
 
 }
 
-function checkOutcome(board, pos){
+function resetGame() {
+    // Clear the board array and reset the visual appearance
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            board[row][col] = '';
+            cells[row * 3 + col].textContent = '';
+        }
+    }
 
+    // Reset game state variables
+    currentPlayer = 'X';
+    gameOver = false;
 }
 
 function swapPlayer(currentPlayer){
