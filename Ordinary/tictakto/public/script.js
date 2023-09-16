85a458493a08884d31fdfbe4cbe65386bde99abd
Ljf227 <ljfrotton@gmail.com>
2023-09-15 20:25:30 -0400

    tic tac toe works now

--- Ordinary/tictakto/public/script.js
+++ Ordinary/tictakto/public/script.js
@@ -1,104 +1,102 @@
-
-// here is the empty board
+// Initialize the empty board
 const board = [
-['', '', ''],
-['', '', ''],
-['', '', '']
-]; 
-
-let currentPlayer = 'X'; 
-let GameOver = true; 
-
-const cells = document.querySelectorAll('.board td'); 
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-
-for (let row = 0; row < board.length; row++) {
-    for (let col = 0; col < board[row].length; col++) {
-        const cell = cells[row * 3 + col];
-        cell.textContent = board[row][col];
-    }
-}
-
-const resetButton = document.getElementById('reset-button');
-resetButton.addEventListener('click', resetGame);
-
-
-function handleCellClick(event){
-    const cell = event.target; 
-    const row = cell.parentElement.rowIndex; 
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  
+  let currentPlayer = 'X';
+  let gameOver = false;
+  
+  const cells = document.querySelectorAll('.board td');
+  cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+  
+  // Initialize the game board on page load
+  updateBoard();
+  
+  const resetButton = document.getElementById('reset-button');
+  resetButton.addEventListener('click', resetGame);
+  
+  function handleCellClick(event) {
+    if (gameOver) return; // Don't allow moves after the game is over
+    const cell = event.target;
+    const row = cell.parentElement.rowIndex;
     const col = cell.cellIndex;
-    if(!canPut(board, row, col)){
-        alert("Cannot place go there!!"); 
-        console.log('path1')
-    }else{
-        console.log('path2')
-        const winner = checkOutcome(board, row, col); 
-        if(winner){
-            if(winner === 'X'){
-                alert("X is the winner"); 
-                resetGame(); 
-            }else{
-                alert("O is the winner"); 
-                resetGame(); 
-            }
-            
-        }else{
-            cell.textContent = currentPlayer; 
-            currentPlayer = swapPlayer(currentPlayer); 
-        }
+  
+    if (!canPut(board, row, col)) {
+      alert("Cannot place 'X' or 'O' there!!");
+    } else {
+      board[row][col] = currentPlayer;
+      cell.textContent = currentPlayer;
+  
+      const winner = checkOutcome(board, row, col);
+      if (winner) {
+        alert(`${winner} is the winner`);
+        gameOver = true;
+      } else {
+        currentPlayer = swapPlayer(currentPlayer);
+      }
     }
-
-}
-
-function canPut(board, row, col){
-    return board[row][col] === ''; 
-}
-
-function checkWinnerPrim(player, board, row, col){
-
-}
-
-function checkWinner(board, row, col){
-    // check X
-    return null;
-
-    // check O
-
-    if(out1){
-
+  }
+  
+  function canPut(board, row, col) {
+    return board[row][col] === '';
+  }
+  
+  function checkWinner(board, player) {
+    // Check rows, columns, and diagonals for a win
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] === player && board[i][1] === player && board[i][2] === player ||
+        board[0][i] === player && board[1][i] === player && board[2][i] === player
+      ) {
+        return true;
+      }
     }
-
-    else if(out2){
-
-    }else{
-
+  
+    if (
+      board[0][0] === player && board[1][1] === player && board[2][2] === player ||
+      board[0][2] === player && board[1][1] === player && board[2][0] === player
+    ) {
+      return true;
     }
-}
-
-function checkOutcome(board, row, col){
-
-    return checkWinner(board, row, col); 
-
-}
-
-function resetGame() {
+  
+    return false;
+  }
+  
+  function checkOutcome(board, row, col) {
+    if (checkWinner(board, currentPlayer)) {
+      return currentPlayer;
+    } else if (board.flat().every(cell => cell !== '')) {
+      alert("It's a draw!");
+      gameOver = true;
+    }
+  }
+  
+  function resetGame() {
     // Clear the board array and reset the visual appearance
     for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            board[row][col] = '';
-            cells[row * 3 + col].textContent = '';
-        }
+      for (let col = 0; col < board[row].length; col++) {
+        board[row][col] = '';
+        cells[row * 3 + col].textContent = '';
+      }
     }
-
+  
     // Reset game state variables
     currentPlayer = 'X';
     gameOver = false;
-}
-
-function swapPlayer(currentPlayer){
-    if(currentPlayer == 'X'){
-        return 'O'; 
-    }else{
-        return 'X'; 
+  }
+  
+  function swapPlayer(currentPlayer) {
+    return currentPlayer === 'X' ? 'O' : 'X';
+  }
+  
+  function updateBoard() {
+    // Update the visual appearance of the board based on the board array
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length; col++) {
+        cells[row * 3 + col].textContent = board[row][col];
+      }
     }
-}
+  }
+  
