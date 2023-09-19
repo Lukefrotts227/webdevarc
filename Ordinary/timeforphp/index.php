f8657a0a1c5061a436ec0509a0febd48e16585ee
Ljf227 <ljfrotton@gmail.com>
2023-09-19 08:45:17 -0400

    my php app is ran in xampp server so it will not work by simply running

---
+++ Ordinary/timeforphp/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Basic PHP Website</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a basic PHP website.</p>
+    <p>Today's date is <?php echo date('Y-m-d'); ?></p>
+    <!-- Add more PHP code and HTML content as needed -->
+</body>
+</html>
