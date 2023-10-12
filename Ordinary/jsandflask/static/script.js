3cbe282ddba9ef70ae27bc52b588848903fe7897
Ljf227 <ljfrotton@gmail.com>
2023-10-12 18:28:51 -0400

    my thing

--- Ordinary/jsandflask/static/script.js
+++ Ordinary/jsandflask/static/script.js
@@ -1,12 +1,2 @@
 
-const containedTitle = document.getElementById('title-container'); 
-
-function animfun(element, animation){
-    element.classList.add(animation); 
-}
-containedTitle.addEventListener('click', function () {
-    animfun(containedTitle, 'pulse');
-    console.log('did it!!'); 
-  });
-
 
