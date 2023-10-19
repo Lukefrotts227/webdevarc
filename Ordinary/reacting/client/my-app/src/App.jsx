957d652b899647ef633f933fb11389eb8f5b1b9c
Ljf227 <ljfrotton@gmail.com>
2023-10-19 12:48:00 -0400

    used vite for this which is nice got tailwind to work as well

---
+++ Ordinary/reacting/client/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className ="">
+        <h1 className="text-5xl text-center text-extrabold">Testing tailwind</h1>  
+      </div>
+    </>
+  )
+}
+
+export default App
