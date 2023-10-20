f1fb0f8c20c96ea3552b215ec5661821b4d87d47
Ljf227 <ljfrotton@gmail.com>
2023-10-20 08:44:01 -0400

    my commit is here

--- Ordinary/reacting/client/my-app/src/App.jsx
+++ Ordinary/reacting/client/my-app/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
+import LandingPage from './components/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <LandingPage />
     </>
   )
 }
--- Ordinary/reacting/client/my-app/src/components/LandingPage.jsx
+++ Ordinary/reacting/client/my-app/src/components/LandingPage.jsx
@@ -5,7 +5,13 @@ export default function LandingPage(){
 
     return(
         <>
-            <main> 
+            <main className = " grid place-items-center min-h-screen bg-gradient-to-l from-slate-900 to-slate-500"> 
+
+                <section className="h-44 bg-black"> 
+                    <header className = "text-4xl text-center text-zinc-100 p-20">
+                        <h1>Welcome to my Page</h1>
+                    </header>
+                </section>
                 
             </main>
         </>
